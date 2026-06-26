2a6a5c9794f5fc72b1c0a018e442dda293fa22fd
tiannian <dtiannian@gmail.com>
2026-06-26 11:12:36 +0800

    fix examples

--- actor/examples/preferred.rs
+++ actor/examples/preferred.rs
@@ -60,7 +60,7 @@ impl Handler<Ping> for PreferredService {
 
 #[tokio::main]
 async fn main() {
-    let (addr, run) = Context::new().run(PreferredService::default(), None);
+    let (addr, run) = Context::new().run(PreferredService, None);
     let service_handle = tokio::spawn(run);
 
     let addr_for_slow = addr.clone();
